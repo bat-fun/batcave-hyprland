196001fe8fdf18ae40248030286d3ac159e323f6
Batman <batman@localhost>
2026-09-05 17:18:53 +0530

    tiny change in moniter.lau

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -5,7 +5,7 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output   = "",
-    mode     = "1920x1080",
+    mode     = "",
     position = "auto",
     scale    = "auto",
 })
